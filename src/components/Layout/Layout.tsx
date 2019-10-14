cc0665a4df9dc3f908e11d9d740911d1d7debff7
Miroslav Gajdos <gjdmiroslav@gmail.com>
2019-10-14 13:27:39 +0200

    bring back awesome react logo

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import logo from '../../logo.svg'
 import './Layout.css'
 
 const Layout: React.FC = props => {
   return (
     <div className="App">
-      <header className="App-header">{props.children}</header>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Build your sentence with React-Redux</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github
+        </a>
+        {props.children}
+      </header>
     </div>
   )
 }
